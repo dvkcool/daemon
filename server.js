0e04ff13e391a23e77cb20558af1d82f3d9de0fa
Divyanshu Kumar <divyanshukumarg@gmail.com>
2018-10-13 13:00:09 +0530

    trial after port change

--- server.js
+++ server.js
@@ -4,26 +4,14 @@ var request = require('request');
 var router = express.Router();
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var exphbs  = require('express-handlebars');
 var alertnode = require('alert-node');
-const vision = require('@google-cloud/vision');
 require('request-debug')(request);
 var fetch = require('fetch');
-app.engine('handlebars', exphbs({
-	defaultLayout: 'main',
-	helpers: {
-	    toJSON : function(object) {
-	      return JSON.stringify(object, null, 4);
-	    }
-  	}
-	})
-);
-app.set('view engine', 'handlebars');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static(__dirname + '/public'));
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
